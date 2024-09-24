46dd544cb65ac8146d95c5da69c232604fcb94a7
ropernj <26ropernj@marshallisd.com>
2024-09-24 09:10:36 -0500

    Add files via upload

---
+++ MyFirstApp/Calculator.cs
@@ -0,0 +1,15 @@
+namespace MyCalculatorApp
+{
+    public class MyCalculatorApp1
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        } 
+        public int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+
+}
